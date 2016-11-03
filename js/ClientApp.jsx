645954f21268847275505caaec10df657dc2b941
Jillian Somera <jsomera@gmail.com>
2016-11-03 15:33:52 -0500

    Set up webpack watch

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -8,7 +8,7 @@ const App = function () {
       <div className='home-info'>
         <h1 className='title'>svideo</h1>
           <input className='search' type='text' placeholder='Search' />
-          <button className='browse-all'> or Browse All </button>
+          <button className='browse-all'> or Browse All?!?!?! </button>
       </div>
     </div>
   )
