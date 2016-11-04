af474f7031546be52095d649059e90f068a33064
Jillian Somera <jsomera@gmail.com>
2016-11-03 19:21:01 -0500

    Create Search component

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,6 +1,7 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
 const Landing = require('./Landing')
+const Search = require('./Search')
 const { Router, Route, hashHistory } = require('react-router')
 // longhand version:
 // const ReactRouter = require('react-router')
@@ -12,6 +13,7 @@ const App = function () {
   return (
     <Router history={hashHistory}>
       <Route path='/' component={Landing} />
+      <Route path='/search' component={Search} />
     </Router>
   )
 }
